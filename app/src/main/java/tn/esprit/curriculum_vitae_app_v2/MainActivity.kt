9292120138a60700f3817a973aa41acdc19c9e3c
jaouayoussef <youssef.jaoua@esprit.tn>
2022-10-10 01:20:36 +0100

    final

--- app/src/main/java/tn/esprit/curriculum_vitae_app_v2/MainActivity.kt
+++ app/src/main/java/tn/esprit/curriculum_vitae_app_v2/MainActivity.kt
@@ -2,71 +2,84 @@ package tn.esprit.curriculum_vitae_app_v2
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.util.Patterns.EMAIL_ADDRESS
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
-    private lateinit var fullName: String
-    private lateinit var age: String
-    private lateinit var email: String
-    private lateinit var gender: String
+    private lateinit var fullName:Editable
+    private lateinit var age:Editable
+    private lateinit var email: Editable
+    private lateinit var  gender: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fullName = fullNameField.editText?.text.toString()
-        age = ageField.editText?.text.toString()
-        email = emailField.editText?.text.toString()
+        fullName = fullNameField.editText?.text!!
+        age = ageField.editText?.text!!
+        email = emailField.editText?.text!!
 
         nextButton.setOnClickListener {
             checkUserInput()
+
         }
     }
 
-    private toast(message: String)
-    {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    private fun checkSelectedGender():String{
+
+        if(maleRadioButton.isChecked){
+
+            gender = maleRadioButton.text.toString()
+
+        }
+        if(femaleRadioButton.isChecked){
+            gender= femaleRadioButton.text.toString()
+
+        }
+
+        return  gender
+
+
     }
+    private fun Toast (message: String) {
 
-    private fun checkUserInput() {
-        val fullName = fullNameField.editText?.text.toString()
-        val age = ageField.editText?.text.toString()
-        bitmap = profileImage.drawToBitmap()
-        val email = emailField.editText?.text.toString()
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
 
-
-        if (fullName.isEmpty()) {
+    }
+    private fun checkUserInput(){
+        if(fullName.isEmpty()){
             fullNameField.error = "Please enter your full name"
-            fullNameField.requestFocus()
             return
         }
-        if (age.isEmpty()) {
+        if(age.isEmpty()){
             ageField.error = "Please enter your age"
-            ageField.requestFocus()
             return
         }
-        if (email.isEmpty()) {
+        if(email.isEmpty()){
             emailField.error = "Please enter your email"
-            emailField.requestFocus()
             return
         }
-        if (!EMAIL_ADDRESS.matcher(email).matches()) {
+        if(!EMAIL_ADDRESS.matcher(email).matches()){
             emailField.error = "Please enter a valid email"
-            emailField.requestFocus()
             return
-        } else {
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("fullName", fullName)
-            intent.putExtra("age", age)
-            intent.putExtra("email", email)
-            intent.putExtra("gender", gender);
+        }
+        if(!maleRadioButton.isChecked && !femaleRadioButton.isChecked) {
+            Toast("please select your gender")
+        }
+        else {
 
-            startActivity(intent)
+
+            val i = Intent(this, SecondActivity::class.java)
+
+            i.putExtra("key_name",fullName.toString())
+            i.putExtra("key_age",age.toString())
+            i.putExtra("key_email",email.toString())
+            i.putExtra("key_gender",checkSelectedGender())
+
+            startActivity(i)
         }
     }
 }
--- app/src/main/java/tn/esprit/curriculum_vitae_app_v2/splashscreen.kt
+++ app/src/main/java/tn/esprit/curriculum_vitae_app_v2/splashscreen.kt
@@ -1,15 +1,16 @@
 package tn.esprit.curriculum_vitae_app_v2
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 
-class splashscreen : appcompatactivity() {
-    override fun onCreate(savedInstanceState: bundle?) {
-        super.oncreate(savedInstanceState)
-        setcontentview(R.layout.splashscreen)
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             val intent = Intent(this, MainActivity::class.java)
@@ -19,6 +20,3 @@ class splashscreen : appcompatactivity() {
     }
 }
 
-
-}
-}
